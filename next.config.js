3580ab159233b68897cbd0090ba5f2dca53b0ca8
Ashton Hefley <ajdhefley@gmail.com>
2022-07-25 22:33:29 -0500

    Make changes for docker

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['rcdb.com']
   }
 }
-
-module.exports = nextConfig
