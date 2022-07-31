2ebd431dbb21ed456e9622076ca15c1099b65362
Ashton Hefley <ajdhefley@gmail.com>
2022-07-31 12:32:21 -0500

    Use vertical bar as separator symbol

--- components/Separator/Separator.tsx
+++ components/Separator/Separator.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import classes from './Separator.module.scss'
 
 /**
  * 
  **/
 interface SeparatorProps {
-
+    /**
+     * Custom styling.
+     **/
+     verticalAlign?: 'baseline'
+        | 'bottom'
+        | 'middle'
+        | 'sub'
+        | 'super'
+        | 'text-bottom'
+        | 'text-top'
+        | 'top'
+        | undefined;
 }
 
 /**
  * Inline symbol separating unrelated pieces of information on the same line.
  **/
-export const Separator: React.FC<SeparatorProps> = () => {
-    return <span>{String.fromCharCode(8901)}</span>
+export const Separator: React.FC<SeparatorProps> = ({ verticalAlign }) => {
+    return <span className={classes.separator} style={{ verticalAlign }}>{String.fromCharCode(124)}</span>
 }
