1805f8fa462b351dc9418881cecc799c8a07f38d
Ashton Hefley <ajdhefley@gmail.com>
2023-02-09 17:08:48 -0600

    remove console log from graphql-client.ts

--- graphql-client.ts
+++ graphql-client.ts
@@ -4,7 +4,7 @@ export const PublicGraphQLClient = new ApolloClient({
     uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
     cache: new InMemoryCache()
 })
-console.log(process.env.NEXT_SERVER_API_URL)
+
 export const ServerGraphQLClient = new ApolloClient({
     uri: `${process.env.NEXT_SERVER_API_URL}/graphql`,
     cache: new InMemoryCache()
